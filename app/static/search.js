86353b7af9339742bd2eda2a4f3413ba02b2725b
Matt O'Hagan <mattohagan@wg-dhcp174d190d072.secure.wireless.fsu.edu>
2016-10-18 17:45:41 -0400

    create stock graph redirect, create gitignore

---
+++ app/static/search.js
@@ -0,0 +1,6 @@
+$("#ticker-search").on('keyup', function (e) {
+    if (e.keyCode == 13) {
+    	var ticker = $('#ticker-search').val().toUpperCase();
+        window.location.href = '/stock/' + ticker;
+    }
+});
