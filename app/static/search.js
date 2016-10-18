d41b2f9b48022901db6d9eea47fe3b2ae970c421
Matt O'Hagan <mattohagan@wg-dhcp174d190d072.secure.wireless.fsu.edu>
2016-10-18 18:10:20 -0400

    add logo, fix styling

--- app/static/search.js
+++ app/static/search.js
@@ -1,6 +1,9 @@
-$("#ticker-search").on('keyup', function (e) {
-    if (e.keyCode == 13) {
-    	var ticker = $('#ticker-search').val().toUpperCase();
-        window.location.href = '/stock/' + ticker;
-    }
+
+$(document).ready(function() {
+	$("#ticker-search").on('keyup', function (e) {
+	    if (e.keyCode == 13) {
+	    	var ticker = $('#ticker-search').val().toUpperCase();
+	        window.location.href = '/stock/' + ticker;
+	    }
+	});
 });
