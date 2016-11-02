37b097f5e9565deb98bba029b3c93874362ab276
Josh Garlitos <joshgarlitos@gmail.com>
2016-11-01 20:29:52 -0400

    Install on webserver

--- app/app.py
+++ app/app.py
@@ -51,5 +51,4 @@ def graph(symbol):
   return render_template("graph.html", div=div, script=script)
 
 if __name__ == "__main__":
-  app.run(host="0.0.0.0", port=5000, debug=True)
-
+  app.run(host='0.0.0.0')
--- app/views.py
+++
@@ -1,7 +0,0 @@
-from flask import render_template
-from app import V_app
-
-@V_app.route('/')
-@V_app.route('/index')
-def index():
-	return render_template('index.html')
