fa40cdc8c3e865458a49c528dd107642d73382be
Matt O'Hagan <mattohagan@Matts-MacBook-Pro-2.local>
2016-11-08 05:04:07 -0500

    add funcitonality for playback or static with url parameter

--- app/static/js/chart.js
+++ app/static/js/chart.js
@@ -36,6 +36,7 @@ var companies = {
 }
 
 let play = true;
+let playbackMode = false;
 let lastTweenValue = 0;
 let stockPairs = [];
 let lastPrice = 0;
@@ -53,6 +54,27 @@ $(function () {
         var date = new Date(element.date).getTime();
         stockPairs.push([date, parseInt(element.values.open)]);
     }
+
+    let query = window.location.search.substring(1);
+    let pair = query.split('=');
+    console.log(pair);
+    if(pair[0] == 'p' && pair[1] == '1') {
+        playbackMode = true;
+    }
+
+    renderStockChart(stockPairs);
+
+    $('#play-pause').on('click', function(){
+        console.log('click');
+        if(play){
+            play = false;
+            $('#ct-stocks path').velocity('stop', true);
+        } else{ 
+            play = true;
+            animateStockChart();
+        }
+    });
+
 
     /* highcharts stuff 
 
@@ -74,19 +96,6 @@ $(function () {
 
     }); */
 
-    renderStockChart(stockPairs);
-
-    $('#play-pause').on('click', function(){
-        console.log('click');
-        if(play){
-            play = false;
-            $('#ct-stocks path').velocity('stop', true);
-        } else{ 
-            play = true;
-            animateStockChart();
-        }
-    });
-
 });
 
 
@@ -101,6 +110,8 @@ function renderStockChart(stock){
         
         data.series[0].push(stock[i][1]);
     }
+
+    console.log(data);
 
     // create graph
     var options = {
@@ -145,7 +156,14 @@ function renderStockChart(stock){
             $('#ct-stocks line').css('opacity', '1');
 
             setTimeout(function(){
-                animateStockChart();
+                if(playbackMode) {
+                    animateStockChart();
+                } else {
+                    // update text since it won't be updated on playback
+                    let last = stockPairs[stockPairs.length - 1];
+                    $('#shown-date').text(formatDate(last[0]));
+                    $('#price').text(last[1].toFixed(2));
+                }
                 $('#ct-stocks path').css('opacity', '1');
             }, 100);
         }, 200);
@@ -185,10 +203,10 @@ function renderStockChart(stock){
           $tooltip.hide();
         }); */
     });
-
+    
+    /*
     // On each drawn element by Chartist we use the Chartist.Svg API to trigger SMIL animations
     chart.on('draw', function(data) {
-        // set accent color
       if(data.type === 'point') {
         // If the drawn element is a line we do a simple opacity fade in. This could also be achieved using CSS3 animations.
         data.element.animate({
@@ -204,9 +222,9 @@ function renderStockChart(stock){
           }
         });
       } 
-    });
+      
+    }); */
 
-    $('#ct-stocks').addClass('show');
 }
 
 
@@ -215,7 +233,7 @@ function animateStockChart(){
     var path = $('#ct-stocks path').get(0);
     var pathLen = path.getTotalLength();
 
-    let duration = 10000;
+    let duration = 9000;
     let durationDiff = duration * (1 - lastTweenValue);
     // could try easing: 'easeInOut'
 
@@ -231,26 +249,32 @@ function animateStockChart(){
 
                 // update path length
                 var adjustedLen = tweenValue * pathLen;
+
                 $('path').attr('opacity', 1);
                 path.setAttribute('stroke-dasharray', adjustedLen+' '+pathLen);
 
                 // update price number
-                let obj = getCurrentStockAndDate(tweenValue);
+                let obj = getCurrentStockAndDate(lastTweenValue);
                 let price = obj.price.toFixed(2);
                 $('#price').text(price);
 
-                if(lastPrice < price) {
+                if(obj.nextPrice >= price) {
                     $('.status').addClass('up');
                     $('.status').removeClass('down');
                 } else {
                     $('.status').addClass('down');
                     $('.status').removeClass('up');
-                }
+                } 
+
+                /* re-use this when we get it accurate
+                else {
+                    $('.status').removeClass('up');
+                    $('.status').removeClass('down');
+                } */
 
                 lastPrice = price;
 
-                let date = moment(obj.date).format('MMM Do, YYYY');
-                console.log(date);
+                let date = formatDate(obj.date);
                 $('#shown-date').text(date);
             }
 
@@ -266,21 +290,32 @@ function animateStockChart(){
     });
 }
 
+// function incase we want to change later
+function formatDate(time) {
+    return moment(time).format('MMM Do, YYYY');
+}
+
 // uses global stockPairs array
 function getCurrentStockAndDate(ratio){
     // get the index relative to progress of graph
+    //console.log(ratio);
     let currentIndex = ratio * (stockPairs.length - 1);
+    //console.log(currentIndex);
     let floor = stockPairs[Math.floor(currentIndex)][1];
+    //console.log(floor);
     let ceiling = stockPairs[Math.ceil(currentIndex)][1];
+    //console.log(ceiling);
 
     // estimate current price by using the percentage that it's between the two points on the graph
     let remainder = currentIndex % 1;
     let adjusted = (ceiling - floor) * remainder;
     let price = floor + adjusted;
 
+    let nextPrice = ceiling;
+
     let date = stockPairs[Math.floor(currentIndex)][0];
 
-    return {price: price, date: date};
+    return {price: price, date: date, nextPrice: nextPrice};
 }
 
 
