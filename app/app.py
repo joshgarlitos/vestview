d271fcc59b09066eba088265869aad60f5686bee
William Wagner <wcw13@my.fsu.edu>
2016-11-10 03:17:41 -0500

    Overhauls ymongo to allow for updating and insertion of stock data, adds command line interface to ymongo, ensures continuous time series, and changes mongodb schema

--- app/app.py
+++ app/app.py
@@ -1,8 +1,9 @@
 from flask import Flask, render_template, jsonify
+from datetime import datetime
 import sys
 sys.path.append('../lib')
 from yfinance import *
-from ymongo import *
+from yfmongo import *
 
 MONGODB_HOST = "localHost"
 DBS_NAME = "vestview"
@@ -13,42 +14,40 @@ app = Flask(__name__)
 
 @app.route('/')
 def root():
-  """
-  This will serve the homepage template
-  The data JSON obj is sent to the client side for real-time
-  autocomplete data
-  """
-  DJIA = ["MMM", "AXP", "AAPL", "BA", "CAT", "CVX", "CSCO", "KO", "DIS",
-        "DD", "XOM", "GE", "GS", "HD", "IBM", "INTC", "JNJ", "JPM",
-        "MCD", "MRK", "MSFT", "NKE", "PFE", "PG", "TRV", "UTX", "UNH",
-        "VZ", "V", "WMT"]
-  yf = YFinance()
+	"""
+	This will serve the homepage template
+	The data JSON obj is sent to the client side for real-time
+	autocomplete data
+	"""
+	DJIA = ["MMM", "AXP", "AAPL", "BA", "CAT", "CVX", "CSCO", "KO", "DIS",
+				"DD", "XOM", "GE", "GS", "HD", "IBM", "INTC", "JNJ", "JPM",
+				"MCD", "MRK", "MSFT", "NKE", "PFE", "PG", "TRV", "UTX", "UNH",
+				"VZ", "V", "WMT"]
 
-  data = yf.get_quote(DJIA)
+	data = get_quote(DJIA)
 
-  return render_template("index.html", autocompleteData=data)
+	return render_template("index.html", autocompleteData=data)
 
 @app.route("/chart/<symbol>")
 def graph(symbol):
-  """
-  This function essentialy serves the page for http://vestview.com/stock/<SYMBOL>
+	"""
+	This function essentialy serves the page for http://vestview.com/stock/<SYMBOL>
 
-  TODO: Add multiple stock functionality, make graph more interactive, and update
-  graph.html template
-  """
-  ym = YMongo(DBS_NAME, COLLECTION_NAME)
-
-  #returns JSON obj
-  stock_data = ym.get_stock(symbol)
-
-  return render_template("chart.html", data=stock_data, symbol=symbol)
+	TODO: Add multiple stock functionality, make graph more interactive, and update
+	graph.html template
+	"""
+	yfm = YFMongo(DBS_NAME, COLLECTION_NAME)
+	#returns list obj
+	daily_data = yfm.get_stock_data(symbol)
+	print("new data")
+	return render_template("chart.html", data=daily_data, symbol=symbol)
 
 
 
 @app.route("/graph")
 def test():
-  return render_template("graph.html")
+	return render_template("graph.html")
 
 if __name__ == "__main__":
-  app.run(host="0.0.0.0", port=5000, debug=True)
+	app.run(host="0.0.0.0", port=5000, debug=True)
 
--- lib/yfinance.py
+++ lib/yfinance.py
@@ -1,6 +1,6 @@
-import json 
+import json
 import requests
-
+from datetime import datetime, date
 PUBLIC_API_URL = 'http://query.yahooapis.com/v1/public/yql?'
 DATATABLES_URL = 'store://datatables.org/alltableswithkeys'
 HISTORICAL_URL = 'http://ichart.finance.yahoo.com/table.csv?s='
@@ -15,76 +15,88 @@ YQL_BASE_QUERY =  "SELECT * FROM {table} WHERE {key} IN {symbols}"
 YQL_HISTORICAL_QUERY =  "SELECT * FROM {table} WHERE {key} in {symbols} and "\
                         "startDate={start_date} and endDate={end_date}"
 
-class YFinance():
+
+
+def get_quote(symbols):
     """
-    Provides an interface to the Yahoo Finance API.
+    Queries the public Yahoo Finance API for quotes.
+
+    Parameters
+    ----------
+    symbols : list-like
+        The list of symbols to get quotes for
+    Returns
+    -------
+    List of JSON objects, with each JSON object corresponding to a symbol.
     """
-    def __init__(self):
-        pass
 
-    def get_quote(self, symbols):
-        """
-        Queries the public Yahoo Finance API for quotes.
+    # have to format symbols list to from ("SYM1", "SYM2", .... ,"SYMN")
+    symbols = "(" + ",".join(['\"' + s.upper() + '"' for s in symbols]) + ")"
+    yql = YQL_BASE_QUERY.format(table=FINANCE_TABLES["quotes"], key="symbol",
+                                symbols=symbols)
+    payload = {
+        "q": yql,
+        "format": "json",
+        "env": ALL_DATATABLES_URL
+    }
+    try:
+        resp = requests.get(PUBLIC_API_URL, params=payload)
+        resp.raise_for_status()
+    except requests.exceptions.RequestException as e:
+        print(e)
 
-        Parameters
-        ----------
-        symbols : list-like
-            The list of symbols to get quotes for
-        Returns
-        -------
-        List of JSON objects, with each JSON object corresponding to a symbol.
-        """
+    return json.loads(resp.text)["query"]["results"]["quote"]
 
-        # have to format symbols list to from ("SYM1", "SYM2", .... ,"SYMN")
-        symbols = "(" + ",".join(['\"' + s.upper() + '"' for s in symbols]) + ")"
-        yql = YQL_BASE_QUERY.format(table=FINANCE_TABLES["quotes"], key="symbol",
-                                    symbols=symbols)
-        payload = {
-            "q": yql,
-            "format": "json",
-            "env": ALL_DATATABLES_URL
-        }
-        try:
-            resp = requests.get(PUBLIC_API_URL, params=payload)
-            resp.raise_for_status()
-        except requests.exceptions.RequestException as e:
-            print(e)
-        
-        return json.loads(resp.text)["query"]["results"]["quote"]
+def get_historical(symbols, start_dt, end_dt):
+    """
+    Queries Yahoo Finance API for daily historical quotes (in the specified range).
+    This data includes daily high, lows, and opening prices.
 
-    def get_historical(self, symbols, start_dt, end_dt):
-        """
-        Queries Yahoo Finance API for daily historical quotes (in the specified range). 
-        This data includes daily high, lows, and opening prices.
+    Parameters
+    ----------
+    symbols : list-like
+        List of symbols to get daily quotes for
+    start_dt : string ("yyyy-mm-dd")
+        The start of the range, inclusive
+    end_dt : string ("yyyy-mm-dd")
+        The end of the range, inclusive
+    """
+    if isinstance(start_dt, date) or isinstance(start_dt, datetime):
+        start_dt = start_dt.strftime("%Y-%m-%d")
+    if isinstance(end_dt, date) or isinstance(end_dt, datetime):
+        end_dt = end_dt.strftime("%Y-%m-%d")
 
-        Parameters
-        ----------
-        symbols : list-like
-            List of symbols to get daily quotes for
-        start_dt : string ("yyyy-mm-dd")
-            The start of the range, inclusive
-        end_dt : string ("yyyy-mm-dd")
-            The end of the range, inclusive
-        """
-        symbols = "(" + ",".join(['\"' + s.upper() + '"' for s in symbols]) + ")"
-        yql = YQL_HISTORICAL_QUERY.format(table=FINANCE_TABLES["historical"], key="symbol",
-                                    symbols=symbols, start_date='"' + start_dt + '"',
-                                    end_date= '"' + end_dt + '"')
+    symbols = "(" + ",".join(['\"' + s.upper() + '"' for s in symbols]) + ")"
+    yql = YQL_HISTORICAL_QUERY.format(table=FINANCE_TABLES["historical"], key="symbol",
+                                symbols=symbols, start_date='"' + start_dt + '"',
+                                end_date= '"' + end_dt + '"')
+    if start_dt == end_dt:
+        print("start_dt and end_dt are equal, so returning")
+        return
 
-        payload = {
-            "q": yql,
-            "format": "json",
-            "env": ALL_DATATABLES_URL
-        }
-        try:
-            resp = requests.get(PUBLIC_API_URL, params=payload)
-            resp.raise_for_status()
-        except requests.exceptions.RequestException as e:
-            print(e)
-        return json.loads(resp.text)["query"]["results"]["quote"]
+    print("Getting historical data from Yahoo Finance API from ", start_dt, " to ", end_dt)
+
+    payload = {
+        "q": yql,
+        "format": "json",
+        "env": ALL_DATATABLES_URL
+    }
+    try:
+        resp = requests.get(PUBLIC_API_URL, params=payload)
+        resp.raise_for_status()
+    except requests.exceptions.RequestException as e:
+        print(e)
+
+    try:
+        ret = json.loads(resp.text)["query"]["results"]["quote"]
+    except TypeError:
+        print("YahooFinance query returned nothing, this is probably due to"
+                         " an invalid date range (e.g. too big, or invalid)")
+        return
+
+    return ret
 
 
 if __name__ == "__main__":
-    yf = YFinance()
-    print(yf.get_quote(["GOOG", "MSFT"]))
-    print(yf.get_historical(["GOOG"], "2015-10-01", "2015-10-10"))
+    print(get_quote(["GOOG", "MSFT"]))
+    print(get_historical(["GOOG"], "2015-10-01", "2015-10-10"))
