0edaf5d1b9f7d06aa0cda1ac71c29bcd599a442a
Hannah McLaughlin <hmm14e@my.fsu.edu>
2016-11-29 03:08:26 -0500

    Add some new styles to homepage

--- app/static/js/search.js
+++ app/static/js/search.js
@@ -26,6 +26,16 @@ $(document).ready(function() {
 				});
 	});
 
+	$("#ticker-search").on('keypress', function(e){
+		//Only execute this when you have selected a company and your
+		//two option buttons are showing
+
+		if($('.catchup').css('display') == 'block'){
+			$(".catchup").css("display", "none");
+			$(".today").css("display", "none");
+		}
+	});
+
 	$("#ticker-search").autocomplete({
 		source: autocompleteData,
 
@@ -34,6 +44,7 @@ $(document).ready(function() {
 
 			// show buttons & hide input bar here
 			$(".catchup").css("display", "block");
+			$(".today").css("display", "block");
 			$(".info-text").css("display", "none");
 
 			//change the size of the input bar
@@ -78,28 +89,26 @@ $(document).ready(function() {
 	minLength:1,
 
 	}).data( "ui-autocomplete" )._renderItem = function( ul, item ) {
+		$(".info-text").css("display", "none");
 		return $( "<li class='ui-menu-item'></li>" )
 		.data('ui-autocomplete-item', item)
 		.append( "<div class='row item-height'><div class='dropdown item-height col-md-12'><div class='ticker-content'>" + item.value + "</div><div class='compName-content'>" + item.name + "</div><div class='price'>" + item.price + "</div></div></div>")
 		.appendTo( ul );
     };
 
-    /*$("#ticker-search").on('keyup', function (e) {
-	    if (e.keyCode == 13) {
-	    	var ticker = $('#ticker-search').val().toUpperCase();
-	        //window.location.href = '/chart/' + ticker;
-	    }
-	});*/
     /* listens for click on search button, someone refactor this*/
-	$("#search-btn").on("click", function(e){
-		var ticker = $('#ticker-search').val().toUpperCase();
-	    window.location.href = '/chart/' + ticker;
-	});
-
+	
 	$(".catchup-button").on("click", function(e){
 		var ticker = $('#ticker-search').val().toUpperCase();
 	    window.location.href = '/chart/' + ticker + "?p=1";
 	});
+
+	$(".today-button").on("click", function(e){
+		var ticker = $('#ticker-search').val().toUpperCase();
+	    window.location.href = '/chart/' + ticker;
+	});
+
+	
 
 });
 
